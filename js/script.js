7ba83e27059009dfdba8e53bc0fcd16e305cf310
Hanna Walgrave <Walgrave.hanna@hotmail.com>
2018-02-22 17:18:56 +0100

    added links minor fixes

--- js/script.js
+++ js/script.js
@@ -1,22 +1,30 @@
 $(document).ready(function() {
 
 
-    $(".icon").click(function(){
-        if($('#myMenu').is(':visible')) {
-            $('#myMenu').hide(500)
+    if (window.matchMedia('(max-width: 768px)').matches)
+    {
+        $(".icon").click(function(){
+            if($('#myMenu').is(':visible')) {
+                $('#myMenu').hide(500)
+                $('.icon').html('<i class="fas fa-bars"></i>');
+            }
+            else {
+                $('#myMenu').show(500);
+                $('.icon').html('<i class="fas fa-times"></i>');
+            }
+        });
+
+        $('#myMenu a').on("click", function(){
+            $('#myMenu').hide(400);
             $('.icon').html('<i class="fas fa-bars"></i>');
+        });
 
-        }
-        else {
-            $('#myMenu').show(500);
-            $('.icon').html('<i class="fas fa-times"></i>');
-        }
-    });
 
-    $('#myMenu a').on("click", function(){
-        $('#myMenu').hide(400);
-        $('.icon').html('<i class="fas fa-bars"></i>');
-    });
+    } else {
+
+    }
+
+
 
     window.onscroll = function() {myFunction()};
 
